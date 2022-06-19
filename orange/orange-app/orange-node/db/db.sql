22bd5b56ad4a0c3fb9f594ce55abb2c4ea362f45
jungkihunm <khkh0130@gmail.com>
2022-06-20 01:14:07 +0900

    jkh db 1차 쿼리

--- orange/orange-app/orange-node/db/db.sql
+++ orange/orange-app/orange-node/db/db.sql
@@ -12,18 +12,18 @@ CONSTRAINT users_pkey PRIMARY KEY (user_no)
 );
 COMMENT ON COLUMN public.users.state IS '0 탈퇴 1 정상 2 계정잠금';
 
---메모
-CREATE TABLE public.note (
-	note_no serial4 NOT NULL,
-	note_data jsonb NULL DEFAULT '{"note_text": [{"content": null, "deadline": null,"note_checked":null}], "note_index": null}'::jsonb,
+--게시판 
+CREATE TABLE public.board (
+	board_no serial4 NOT NULL,
+	board_content TEXT b NULL,
+	board_count integer not null DEFAULT 1,
+	board_title varchar not null,
 	state int4 NOT NULL DEFAULT 1,
 	reg_dt timestamp NOT NULL DEFAULT now(),
-	user_no serial4 NOT NULL,
-    end_dt timestamp NOT NULL DEFAULT now(),
-	CONSTRAINT note_pkey PRIMARY KEY (note_no),
+	user_no serial4 NOT NULL,    
+	CONSTRAINT note_pkey PRIMARY KEY (board_no),
 	CONSTRAINT users_user_no_fkey FOREIGN KEY (user_no) REFERENCES public.users(user_no) ON DELETE CASCADE ON UPDATE CASCADE
 );
 COMMENT ON COLUMN public.note.state IS '0 삭제 1 정상';
 COMMENT ON COLUMN public.note.reg_dt IS '생성시간';
 COMMENT ON COLUMN public.note.end_dt IS '최종수정시간';
-COMMENT ON COLUMN public.note.note_data IS '{"note_text": [{"content": null, "deadline": null,"note_checked":null}], "note_index": null}';
