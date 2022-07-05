6a5bd3074156ddfcad50c0d67ed01dabb127fe90
jungkihunm <khkh0130@gmail.com>
2022-07-05 18:30:00 +0900

    jkh sql edit

--- orange/orange-app/orange-node/db/db.sql
+++ orange/orange-app/orange-node/db/db.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA public;
 create table users(
 user_no serial not null,
 user_id varchar(20) not null,
-user_pw varchar(100) not null,
+user_pw varchar not null,
 user_name varchar(20) not null,
 state integer not null DEFAULT 1,
 reg_dt timestamp not null DEFAULT now(),
@@ -15,7 +15,7 @@ COMMENT ON COLUMN public.users.state IS '0 탈퇴 1 정상 2 계정잠금';
 COMMENT ON COLUMN public.users.user_tokens IS '로그인시 jwt 저장';
 
 --게시판 
-CREATE TABLE public.board (
+CREATE TABLE board (
 	board_no serial4 NOT NULL,
 	board_content TEXT  NULL,
 	board_count integer not null DEFAULT 1,
@@ -23,10 +23,22 @@ CREATE TABLE public.board (
 	state int4 NOT NULL DEFAULT 1,
 	reg_dt timestamp NOT NULL DEFAULT now(),
 	end_dt timestamp NULL,
-	user_no serial4 NOT NULL,    
+	user_no int4 NOT NULL,    
 	CONSTRAINT board_pkey PRIMARY KEY (board_no),
 	CONSTRAINT users_user_no_fkey FOREIGN KEY (user_no) REFERENCES public.users(user_no) ON DELETE CASCADE ON UPDATE CASCADE
 );
 COMMENT ON COLUMN public.board.state IS '0 삭제 1 정상';
 COMMENT ON COLUMN public.board.reg_dt IS '생성시간';
 COMMENT ON COLUMN public.board.end_dt IS '최종수정시간';
+
+-- 사용자 권한
+create table userslevel(
+ul_no serial not null,
+ul_level integer not null,
+state integer not null DEFAULT 1,
+user_no int4 not null,
+CONSTRAINT users_level_pkey PRIMARY KEY (ul_no)
+CONSTRAINT users_user_no_fkey FOREIGN KEY (user_no) REFERENCES public.users(user_no) ON DELETE CASCADE ON UPDATE CASCADE
+);
+COMMENT ON COLUMN public.users.state IS '0 탈퇴 1 정상 2 계정잠금';
+COMMENT ON COLUMN public.users.ul_level IS '0 최고관리자 1 중간관리자 2 고객';
